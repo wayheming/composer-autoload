88943bf62eebfd73798b55a90c474898ad4403d0
wayheming <way.h3ming@gmail.com>
2022-05-31 18:52:13 +0300

    Init

---
+++ composer-autoload.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: аAutoload test
+ * Plugin URI: https://www.mywebsite.com
+ * Description: Just another contact form plugin. Simple but flexible.
+ * Author: Ernest Beginov
+ * Version: 0.0.1
+ * Author URI: https://www.mywebsite.com
+ */
+
+require 'vendor/autoload.php';
+
+new Plugin\Includes\Some_Test();
